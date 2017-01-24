ea08580463601521a5cfa143bc364a611ab9634f
Hiroshi Uyama <bonjamin.jan26@gmail.com>
2017-01-24 14:30:51 +0900

    テストの実装

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  # specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it "購入した本数 0本の時は 0本" do
+    expect(BonusDrink.total_count_for(0)).to eq 0
+  end
+  it "購入した本数 1本の時は 1本" do
+    expect(BonusDrink.total_count_for(1)).to eq 1
+  end
+  it "購入した本数 3本の時は 4本" do
+    expect(BonusDrink.total_count_for(3)).to eq 4
+  end
+  it "購入した本数 11本の時は 16本" do
+    expect(BonusDrink.total_count_for(11)).to eq 16
+  end
+  it "購入した本数 100本の時は ?本" do
+    expect(BonusDrink.total_count_for(100)).to_not eq 100
+  end
 end
