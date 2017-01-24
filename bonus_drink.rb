85378d8ae1b662de55203e1195ca1ae7e779e6b5
Hiroshi Uyama <bonjamin.jan26@gmail.com>
2017-01-24 14:31:14 +0900

    トータルの本数計算

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,8 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    # おまけの数 = ((飲める本数 - 1) / 3).abs.floor
+    # 購入した本数amount + おまけの数 = 飲める本数
+    # で、上の方程式をざっくりで解いたものが↓
+    ((amount * 1.5) - 0.5).abs.floor
   end
 end
